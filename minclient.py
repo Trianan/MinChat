c4b691ca56c97113f928ced09b1466a80ee20517
Tristin Manson <trianan@Tristins-MacBook-Air.local>
2023-05-06 19:14:11 -0400

    Complete rough implementation based on tutorial at https://dev.to/zeyu2001/build-a-chatroom-app-with-python-44fa with modifications.

--- minclient.py
+++ minclient.py
@@ -2,3 +2,94 @@
 # May 6 / 2023
 # By: Trianan
 # -----------------------------------------------------------------------------
+
+import threading, socket, argparse, os
+
+SPLASH_STR = '''
+
+
+ /$$      /$$ /$$            /$$$$$$  /$$                   /$$    
+| $$$    /$$$|__/           /$$__  $$| $$                  | $$    
+| $$$$  /$$$$ /$$ /$$$$$$$ | $$  \__/| $$$$$$$   /$$$$$$  /$$$$$$  
+| $$ $$/$$ $$| $$| $$__  $$| $$      | $$__  $$ |____  $$|_  $$_/  
+| $$  $$$| $$| $$| $$  \ $$| $$      | $$  \ $$  /$$$$$$$  | $$    
+| $$\  $ | $$| $$| $$  | $$| $$    $$| $$  | $$ /$$__  $$  | $$ /$$
+| $$ \/  | $$| $$| $$  | $$|  $$$$$$/| $$  | $$|  $$$$$$$  |  $$$$/
+|__/     |__/|__/|__/  |__/ \______/ |__/  |__/ \_______/   \___/  
+                                                                   
+Version Alpha - TriaNaN Inc.
+--------------------------------------------------------------------------------
+'''
+
+
+class Send(threading.Thread):
+    def __init__(self, skt, name):
+        super().__init__()
+        self.skt = skt
+        self.name = name
+
+    def run(self):
+        while True:
+            message = input(f'{self.name} > ')
+
+            if message == 'QUIT':
+                self.skt.sendall(f'SERVER: {self.name} left the chatroom.'.encode('ascii'))
+                break
+            else:
+                self.skt.sendall(f'{self.name}: {message}'.encode('ascii'))
+        print('\nQuitting MinClient...')
+        self.skt.close()
+        os._exit(0)
+
+
+class Receive(threading.Thread):
+    def __init__(self, skt, name):
+        super().__init__()
+        self.skt = skt
+        self.name = name
+
+    def run(self):
+        while True:
+            message = self.skt.recv(1024)
+            if message:
+                print(f"\r{message.decode('ascii')}\n{self.name} > ", end='')
+            else:
+                print('\nConnection to server lost; contact server admin.\nQuitting MinClient...')
+                self.skt.close()
+                os._exit(0)
+
+
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def start(self):
+        print(SPLASH_STR)
+        print(f'Attempting connection to {self.host}:{self.port}...')
+        self.skt.connect((self.host, self.port))
+        print(f'Successfully connected!\r')
+        name = input('Your name: ')
+        print(f"\rWelcome to MinChat server {self.host}:{self.port} {name}!")
+        print('Getting ready to send and receive messages...')
+
+        send = Send(self.skt, name)
+        receive = Receive(self.skt, name)
+        send.start()
+        receive.start()
+
+        self.skt.sendall(f'SERVER: {name} joined the chatroom.'.encode('ascii'))
+        print("\rYou are ready to chat. Quit the chatroom by typing QUIT.\n")
+        print(f'{name} > ', end='')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Chatroom Server')
+    parser.add_argument('host', help='Interface the server listens at')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+
+client = Client(args.host, args.p)
+client.start()
--- minserver.py
+++ minserver.py
@@ -2,3 +2,86 @@
 # May 6 / 2023
 # By: Trianan
 # -----------------------------------------------------------------------------
+
+import threading, socket, argparse, os
+
+
+class Server(threading.Thread):
+    def __init__(self, host, port):
+        super().__init__()
+        self.connections = []
+        self.host = host
+        self.port = port
+    
+    def run(self):
+        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        skt.bind((self.host, self.port))
+        skt.listen(1)
+        print("Listening at port", skt.getsockname())
+
+        while True:
+            client_skt, client_addr = skt.accept()
+            print(f'Accepted a new connection from {client_skt.getpeername()} to {client_skt.getsockname()}')
+
+            server_skt = ServerSocket(client_skt, client_addr, self) # To be defined.
+            server_skt.start()
+            self.connections.append(server_skt)
+            print(f'Ready to receive messages from {client_skt.getpeername()}')
+    
+    def broadcast(self, message, source):
+        for connection in self.connections:
+            if connection.sockname != source:
+                connection.send(message)
+
+    def remove_connection(self, server_skt):
+        self.connections.remove(server_skt)
+
+
+class ServerSocket(threading.Thread):
+    def __init__(self, sc, sockname, server):
+        super().__init__()
+        self.sc = sc
+        self.sockname = sockname
+        self.server = server
+
+    def run(self):
+        while True:
+            message = self.sc.recv(1024).decode('ascii')
+            if message:
+                print(('{!r}    {}').format(message, self.sockname))
+                self.server.broadcast(message, self.sockname)
+            else:
+                print(f'{self.sockname} has closed their connection.')
+                self.sc.close()
+                server.remove_connection(self)
+                return
+    
+    def send(self, message):
+        self.sc.sendall(message.encode('ascii'))
+
+
+def exit(server):
+    while True:
+        action = input('')
+        if action == 'q':
+            print('Closing all connections...')
+            for connection in server.connections:
+                connection.sc.close()
+            print('Shutting down server...')
+            os._exit(0)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MinServer')
+    parser.add_argument('host', help='Interface the server listens at')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+
+    # Create and start server thread
+    server = Server(args.host, args.p)
+    server.start()
+
+    exit = threading.Thread(target = exit, args = (server,))
+    exit.start()
